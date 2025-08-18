1a75a3519cb0f04b7e40d2588aef7c831ecbf673
DEBORAJ ROY <deborajroy123@gmail.com>
2025-08-19 03:04:32 +0600

    Section 5 - Logout in Action

--- Mango/Mango.Web/Controllers/AuthController.cs
+++ Mango/Mango.Web/Controllers/AuthController.cs
@@ -100,9 +100,11 @@ namespace Mango.Web.Controllers
         }
 
 
-        public IActionResult Logout()
+        public async Task<IActionResult> Logout()
         {
-            return View();
+            await HttpContext.SignOutAsync();
+            _tokenProvider.ClearToken();
+            return RedirectToAction("Index", "Home");
         }
 
 
