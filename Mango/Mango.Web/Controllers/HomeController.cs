343514c67ea4907ee09664238ae176bad5bf84bf
DEBORAJ ROY <deborajroy123@gmail.com>
2025-08-22 00:45:41 +0600

    Section 7 - Details UI

--- Mango/Mango.Web/Controllers/HomeController.cs
+++ Mango/Mango.Web/Controllers/HomeController.cs
@@ -37,7 +37,7 @@ public class HomeController : Controller
     }
 
     [Authorize]
-    public async Task<IActionResult> Details(int productId)
+    public async Task<IActionResult> ProductDetails(int productId)
     {
         ProductDto? model = new();
 
