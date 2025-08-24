08bd06199506746d01244d979a11ed190c4e746f
DEBORAJ ROY <deborajroy123@gmail.com>
2025-08-25 02:15:09 +0600

    Section 10 - MessageBus Interface

---
+++ Mango/Mango.MessageBus/MessageBus.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Mango.MessageBus
+{
+    public class MessageBus : IMessageBus
+    {
+        public Task PublishMessage(object message, string topic_queue_Name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
