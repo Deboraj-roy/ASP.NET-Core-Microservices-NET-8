cb898e6f28d2081148d3fd9dc6bba573f73f4f18
DEBORAJ ROY <deborajroy123@gmail.com>
2025-08-26 01:04:01 +0600

    Section 10 - MessageBus Implementation

--- Mango/Mango.MessageBus/MessageBus.cs
+++ Mango/Mango.MessageBus/MessageBus.cs
@@ -1,11 +1,34 @@
 ﻿
+using Azure.Messaging.ServiceBus;
+using Newtonsoft.Json;
+using System.Text;
+
 namespace Mango.MessageBus
 {
     public class MessageBus : IMessageBus
     {
-        public Task PublishMessage(object message, string topic_queue_Name)
+        //public Task PublishMessage(object message, string topic_queue_Name)
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        private string connectionString = "Endpoint=sb://mangoweb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=HjoslS58pPHtAULb0tay/jx4Ys0+MO5/R+ASbCcFTG0=";
+
+        public async Task PublishMessage(object message, string topic_queue_Name)
         {
-            throw new NotImplementedException();
+            await using var client = new ServiceBusClient(connectionString);
+
+            ServiceBusSender sender = client.CreateSender(topic_queue_Name);
+
+            var jsonMessage = JsonConvert.SerializeObject(message);
+            ServiceBusMessage finalMessage = new ServiceBusMessage(Encoding
+                .UTF8.GetBytes(jsonMessage))
+            {
+                CorrelationId = Guid.NewGuid().ToString(),
+            };
+
+            await sender.SendMessageAsync(finalMessage);
+            await client.DisposeAsync();
         }
     }
 }
