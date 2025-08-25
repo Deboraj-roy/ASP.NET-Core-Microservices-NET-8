f72aca15bcfba0c511ee00f96aa62e7fde7db81d
DEBORAJ ROY <deborajroy123@gmail.com>
2025-08-26 02:09:26 +0600

    Others Options

--- Mango/Mango.MessageBus/IMessageBus.cs
+++ Mango/Mango.MessageBus/IMessageBus.cs
@@ -9,5 +9,10 @@ namespace Mango.MessageBus
     public interface IMessageBus
     {
         Task PublishMessage(object message, string topic_queue_Name);
+
+        Task PublishRabbitAsync(object message);
+        void PublishInMemory(object message);
+        string? ReceiveInMemory();
+        Task PublishAWSAsync(object message, string topicArn, string queueUrl);
     }
 }
