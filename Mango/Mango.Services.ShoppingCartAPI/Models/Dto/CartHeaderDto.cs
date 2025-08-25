0c2d88d5bc3b0e89a2720e0f674540a7b9e335c6
DEBORAJ ROY <deborajroy123@gmail.com>
2025-08-26 01:29:49 +0600

    Section 10 - More properties in Cart

--- Mango/Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
+++ Mango/Mango.Services.ShoppingCartAPI/Models/Dto/CartHeaderDto.cs
@@ -7,6 +7,10 @@
         public string? CouponCode { get; set; }
         public double Discount { get; set; }
         public double CartTotal { get; set; } = 0;
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
 
         public DateTime CreatedDate { get; set; } = DateTime.Now;
         public DateTime UpdatedDate { get; set; } = DateTime.Now;
