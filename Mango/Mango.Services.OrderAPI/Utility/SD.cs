b38b504e74a5dd41e0b1b217232fe809827805f4
DEBORAJ ROY <deborajroy123@gmail.com>
2025-08-30 01:27:09 +0600

    Section 12 - Constants in Order API

--- Mango/Mango.Services.OrderAPI/Utility/SD.cs
+++ Mango/Mango.Services.OrderAPI/Utility/SD.cs
@@ -2,6 +2,18 @@
 {
     public class SD
     {
+        public const string Status_Pending = "Pending";
+        public const string Status_Approved = "Approved";
+        public const string Status_ReadyForPickup = "ReadyForPickup";
+        public const string Status_Completed = "Completed";
+        public const string Status_Refunded = "Refunded";
+        public const string Status_Cancelled = "Cancelled";
+
+        public const string RoleAdmin = "ADMIN";
+        public const string RoleCustomer = "CUSTOMER";
+
+
+
         public static string CouponAPIBase { get; set; }
         public static string AuthAPIBase { get; set; }
         public static string ProductAPIBase { get; set; }
