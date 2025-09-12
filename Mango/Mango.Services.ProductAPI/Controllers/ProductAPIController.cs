b68e74de3d579bee8152d1311814c7bc177ba001
DEBORAJ ROY <deborajroy123@gmail.com>
2025-09-12 18:40:06 +0600

    Section 16 - Update Product Image in Action

--- Mango/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
+++ Mango/Mango.Services.ProductAPI/Controllers/ProductAPIController.cs
@@ -109,15 +109,51 @@ namespace Mango.Services.ProductAPI.Controllers
 
         [HttpPut]
         [Authorize(Roles = "ADMIN")]
-        public ResponseDto Put([FromBody] ProductDto ProductDto)
+        public ResponseDto Put(ProductDto ProductDto)
         {
             try
             {
-                Product obj = _mapper.Map<Product>(ProductDto);
-                _db.Products.Update(obj);
+                Product product = _mapper.Map<Product>(ProductDto);
+
+                if (ProductDto.Image != null)
+                {
+                    if (!string.IsNullOrWhiteSpace(product.ImageLocalPath))
+                    {
+                        var oldImagePath = Path.Combine(Directory.GetCurrentDirectory(), product.ImageLocalPath);
+
+                        FileInfo oldImage = new FileInfo(oldImagePath);
+
+                        if (oldImage.Exists)
+                        {
+                            oldImage.Delete();
+                        }
+                    }
+
+                    string fileName = product.ProductId + Path.GetExtension(ProductDto.Image.FileName);
+                    string filePath = @"wwwroot\ProductImages\" + fileName;
+                    var filePathDirectory = Path.Combine(Directory.GetCurrentDirectory(), filePath);
+                    using (var fileStream = new FileStream(filePathDirectory, FileMode.Create))
+                    {
+                        ProductDto.Image.CopyTo(fileStream);
+                    }
+                    var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.Value}{HttpContext.Request.PathBase.Value}";
+                    //product.ImageUrl = baseUrl + "/ProductImages/" + filePath;
+                    product.ImageUrl = baseUrl + "/ProductImages/" + fileName;
+                    product.ImageLocalPath = filePath;
+                }
+                else if (!string.IsNullOrEmpty(ProductDto.ImageUrl))
+                {
+                    product.ImageUrl = ProductDto.ImageUrl;
+                }
+                else
+                {
+                    product.ImageUrl = "https://placehold.co/600x400";
+                }
+
+                _db.Products.Update(product);
                 _db.SaveChanges();
 
-                _response.Result = _mapper.Map<ProductDto>(obj);
+                _response.Result = _mapper.Map<ProductDto>(product);
             }
             catch (Exception ex)
             {
