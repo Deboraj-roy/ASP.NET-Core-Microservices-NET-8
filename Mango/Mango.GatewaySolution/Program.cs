32b12be4e069a56b7452da56957439786937cd2e
DEBORAJ ROY <deborajroy123@gmail.com>
2025-09-13 23:33:38 +0600

    Section 17 - Configure Application to use Ocelot

--- Mango/Mango.GatewaySolution/Program.cs
+++ Mango/Mango.GatewaySolution/Program.cs
@@ -1,6 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+using System.Text.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+builder.Services.AddOcelot();
+
 
 app.MapGet("/", () => "Hello World!");
-
+app.UseOcelot();
 app.Run();
