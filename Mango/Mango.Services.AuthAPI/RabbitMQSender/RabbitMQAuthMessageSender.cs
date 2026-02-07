b34502896ca58a8301c1461adcef12569154058b
DEBORAJ ROY <deborajroy123@gmail.com>
2026-02-08 01:55:28 +0600

    Section 19 - RabbitMQ Message Sender

---
+++ Mango/Mango.Services.AuthAPI/RabbitMQSender/RabbitMQAuthMessageSender.cs
@@ -0,0 +1,41 @@
+﻿using RabbitMQ.Client;
+
+namespace Mango.Services.AuthAPI.RabbitMQSender
+{
+    public class RabbitMQAuthMessageSender : IRabbitMQAuthMessageSender
+    {
+        private readonly string _hostName;
+        private readonly string _username;
+        private readonly string _password;
+        private IConnection _connection;
+        public RabbitMQAuthMessageSender()
+        {
+            this._hostName = "localhost";
+            this._username = "guest";
+            this._password = "guest";
+        }
+        public void SendMessage(object message, string queueName)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = _hostName,
+                UserName = _username,
+                Password = _password
+            };
+            this._connection = factory.CreateConnection();
+
+            using var channel = connection.CreateModel();
+
+            //channel.QueueDeclare(queue: queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+            //var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            //channel.BasicPublish(exchange: "", routingKey: queueName, basicProperties: null, body: body);
+
+            channel.QueueDeclare(queueName);
+
+            var json = JsonConvert.SerializeObject(message);
+            var body = Encoding.UTF8.GetBytes(json);
+
+            channel.BasicPublish(exchange: "", routingKey: queueName, body: body);
+        }
+    }
+}
